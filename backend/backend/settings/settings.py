f00d09353d703595da104a9485b66f5a3907297c
marscj <mjx_cj@163.com>
2019-11-12 14:30:21 +0400

    commit

--- backend/backend/settings/settings.py
+++ backend/backend/settings/settings.py
@@ -9,14 +9,14 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
-    'app.user',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app.user',
 ]
 
 MIDDLEWARE = [
