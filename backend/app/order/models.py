0a28d0cefa13dee4e0dd6b284f7b40c9fba36b46
marscj <mjx_cj@163.com>
2019-11-27 12:01:11 +0400

    commit

--- backend/app/order/models.py
+++ backend/app/order/models.py
@@ -14,38 +14,55 @@ class Order(models.Model):
         (6, 'Delete'),
     )
 
+    # 订单状态
     status = models.IntegerField(blank=True, null=True, choices=STATUS_SHEET, default=1)
     
+    # 订单类型
     cagetory = models.IntegerField(blank=True, null=True)
 
-    info = models.TextField(blank=True, null=True)
+    # 信息
+    main_info = models.TextField(blank=True, null=True)
 
-    subInfo = models.TextField(blank=True, null=True)
+    # 副信息
+    sub_info = models.TextField(blank=True, null=True)
 
+    # 自定义信息
     other_info = models.TextField(blank=True, null=True)
 
+    # 开始时间
     from_date = models.DateTimeField(blank=True, null=True)
 
+    # 结束时间
     to_date = models.DateTimeField(blank=True, null=True)
 
+    # 地址
     addr = models.TextField(blank=True, null=True)
 
+    # 图片
     image = models.ImageField(blank=True, null=True)
 
+    # 服务码
     code = models.CharField(blank=True, null=True, default=get_random_string(length=4))
 
-    evaluation = models.TextField(blank=True, null=True)
+    # 评价
+    eva_info = models.TextField(blank=True, null=True)
 
-    elv = models.IntegerField(blank=True, null=True)
+    # 评价等级
+    eva_lv = models.IntegerField(blank=True, null=True)
 
+    # 坐标
     lat = models.FloatField(blank=True, null=True)
 
+    # 坐标
     lgt = models.FloatField(blank=True, null=True)
 
+    # 创建时间
     create_at = models.DateTimeField(auto_now_add=True)
 
+    # 修改时间
     change_at = models.DateTimeField(auto_now=True)
 
+    # 客人
     customer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='order', blank=True, null=True)
 
 
