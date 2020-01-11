250bfc3af17de9f1b108e0bbdc6ec484b603dbdf
marscj <mjx_cj@163.com>
2020-01-11 11:02:28 +0400

    commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,9 +16,6 @@ export default {
     return {
       locale: enUS
     }
-  },
-  mounted () {
-
   }
 }
 </script>
@@ -28,8 +25,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
