7572334c877c40a06e0a6b2106ebaa7e63bc997e
marscj <mjx_cj@163.com>
2020-01-11 12:36:58 +0400

    commit

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// ant-design-vue
+import Antd from 'ant-design-vue';
+Vue.use(Antd);
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ frontend/src/router/config.js
@@ -0,0 +1,32 @@
+import {
+  UserLayout,
+  BasicLayout,
+  RouteView,
+  BlankLayout,
+  PageView
+} from "@/layouts";
+
+export const asyncRouterMap = [
+  {
+    path: "/admin",
+    name: "index",
+    component: BasicLayout,
+    meta: { title: "Admin" },
+    redirect: "/admin/order"
+  },
+  {
+    path: "/admin/order",
+    name: "Order",
+    component: PageView,
+    meta: { title: "Order" },
+    redirect: "order-list",
+    children: [
+      {
+        path: "/admin/order-list",
+        name: "OrderList",
+        component: () => import("@/views/order/List"),
+        meta: { title: "OrderList", keepAlive: true, permission: [] }
+      }
+    ]
+  }
+];
