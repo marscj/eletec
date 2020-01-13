be0644f632a82de7bff9609884cae080517f9493
marscj <mjx_cj@163.com>
2020-01-13 15:00:55 +0400

    commit

--- backend/app/user/views.py
+++ backend/app/user/views.py
@@ -25,24 +25,19 @@ class sendSms(views.APIView):
             phone_number = serializers.data['phone_number']
             verify_code = get_random_string(4, '0123456789')
 
-            if settings.DEBUG:
-                response = requests.post(url, data={
-                    'Body':'[Eletec] Your verification code is %s' % verify_code,
-                    'From':'+15005550006', 
-                    'To':phone_number
-                }, auth=(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN))
-            else :
-                response = requests.post(url, data={
-                    'Body':'[Eletec] Your verification code is %s' % verify_code,
-                    'From':'+13473086886',
-                    'To': phone_number
-                }, auth=(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN))
-
+            response = requests.post(url, data={
+                'Body': '[Eletec] Your verification code is %s' % verify_code,
+                'From': settings.TWILIO_FROM_NUMBER,
+                'To': phone_number
+            }, auth=(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN))
+                
             if response.ok:
                 try: 
                     verify = VerifyCode.objects.get(phone_number=phone_number)
+                    verify.verify_code = verify_code
+                    verify.save()
                 except VerifyCode.DoesNotExist:
-                    verify = VerifyCode.objects.create(phone_number=phone_number, code=verify_code)
+                    verify = VerifyCode.objects.create(phone_number=phone_number, verify_code=verify_code)
 
                 return Response(response.json(), status=response.status_code)
             else:
