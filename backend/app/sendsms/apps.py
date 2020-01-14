2313d6de912134b41510f7b0b38315dcee300ed1
marscj <mjx_cj@163.com>
2020-01-14 16:47:32 +0400

    commit

--- backend/app/sendsms/apps.py
+++ backend/app/sendsms/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class SmsConfig(AppConfig):
+class SendSmsConfig(AppConfig):
     name = 'app.sendsms'
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^api/', include('app.sms.urls')),
-    url(r'^api/phone_login/', include('phone_login.urls')),
+
 ]
