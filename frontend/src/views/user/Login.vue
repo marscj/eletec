a62b336eb070fe7a8403e36d03fd8ffb964b8d53
marscj <mjx_cj@163.com>
2020-01-16 15:27:12 +0400

    commit

---
+++ frontend/src/views/user/Login.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="h-screen flex w-full bg-img vx-row no-gutter items-center justify-center"
+    id="page-login"
+  >
+    <div class="vx-col sm:w-1/2 md:w-1/2 lg:w-3/4 xl:w-3/5 sm:m-0 m-4">
+      <vx-card>
+        <div slot="no-body" class="full-page-bg-color">
+          <div class="vx-row no-gutter justify-center items-center">
+            <div class="vx-col hidden lg:block lg:w-1/2">
+              <img
+                src="@/assets/images/pages/login.png"
+                alt="login"
+                class="mx-auto"
+              />
+            </div>
+
+            <div class="vx-col sm:w-full md:w-full lg:w-1/2 d-theme-dark-bg">
+              <div class="px-8 pt-8 login-tabs-container">
+                <div class="vx-card__title mb-4">
+                  <h4 class="mb-4">Login</h4>
+                  <p>Welcome back, please login to your account.</p>
+                </div>
+
+                <login-email></login-email>
+                <!-- <login-user-name > </login-user-name> -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </vx-card>
+    </div>
+  </div>
+</template>
+
+<script>
+// import LoginUserName from "./LoginUserName.vue"
+import LoginEmail from "./LoginEmail.vue";
+
+export default {
+  components: {
+    // LoginUserName,
+    LoginEmail
+  }
+};
+</script>
+
+<style lang="scss">
+.login-tabs-container {
+  min-height: 400px;
+
+  .con-tab {
+    padding-bottom: 14px;
+  }
+
+  .con-slot-tabs {
+    margin-top: 1rem;
+  }
+}
+</style>
