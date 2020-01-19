ac8c880700b21b7122a804f00bb89b75416a6c56
marscj <mjx_cj@163.com>
2020-01-19 13:08:24 +0400

    commit

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,12 +1,4 @@
 const api = {
-  Login: '/auth/login',
-  Logout: '/auth/logout',
-  ForgePassword: '/auth/forge-password',
-  Register: '/auth/register',
-  twoStepCode: '/auth/2step-code',
-  SendSms: '/account/sms',
-  SendSmsErr: '/account/sms_err',
-  // get my info
-  UserInfo: '/user/info'
-}
-export default api
+  Login: "/auth/login"
+};
+export default api;
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -70,6 +70,12 @@ const vueConfig = {
   },
 
   devServer: {
+    port: 8001,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
     proxy: {
       "/api": {
         target: "http://127.0.0.1:8000",
