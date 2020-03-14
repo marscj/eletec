d012c051dd3672c6dfceee84c94786fe3fb10661
marscj <mjx_cj@163.com>
2020-03-14 23:41:17 +0400

    commit

--- backend/authenticate/phone/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from .views import GenerateOTP, ValidateOTP
-
-urlpatterns = [
-    url(r'^auth/phone/generate/$', GenerateOTP.as_view(), name="generate"),
-    url(r'^auth/phone/validate/$', ValidateOTP.as_view(), name="validate"),
-]
