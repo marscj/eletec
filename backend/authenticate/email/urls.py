3f894ebf58f4d9a34ab63451132c215fe1990e04
marscj <mjx_cj@163.com>
2020-03-15 17:25:54 +0400

    commit

--- backend/authenticate/email/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-# from .views import GenerateOTP, ValidateOTP
-
-urlpatterns = [
-    # url(r'^auth/email/generate/$', GenerateOTP.as_view(), name="email_generate"),
-    # url(r'^auth/email/validate/$', ValidateOTP.as_view(), name="email_validate"),
-]
