c6dca6c7c7e05546f03d5cee04a2533280c7ddbf
marscj <mjx_cj@163.com>
2020-03-18 17:57:42 +0400

    commit

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,10 +1,7 @@
-import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 from core.channel.routing import websocket_urlpatterns
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.prod')
 
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
