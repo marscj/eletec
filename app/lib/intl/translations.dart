5b9a3a75778b6e94dcdf743b40cb9dd082274abf
marscj <mjx_cj@163.com>
2020-03-25 15:32:41 +0400

    commit

--- app/lib/intl/translations.dart
+++ app/lib/intl/translations.dart
@@ -36,7 +36,7 @@ class TranslationsDelegate extends LocalizationsDelegate<Translations> {
   const TranslationsDelegate();
 
   @override
-  bool isSupported(Locale locale) => ['en', 'hi'].contains(locale.languageCode);
+  bool isSupported(Locale locale) => ['en', 'ar'].contains(locale.languageCode);
 
   @override
   Future<Translations> load(Locale locale) => Translations.load(locale);
