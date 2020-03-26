a1065764e649d101579c673f89fed18efc2ca72e
marscj <mjx_cj@163.com>
2020-03-26 22:28:32 +0400

    commit

--- app/lib/view/setting/view.dart
+++ app/lib/view/setting/view.dart
@@ -3,7 +3,7 @@ import 'package:eletec/locale/locale_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatelessWidget {
+class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     void onPressed() {
