1635008d17e902f3db21b363731567e2131cb98a
marscj <mjx_cj@163.com>
2020-03-31 17:33:35 +0400

    commit

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:eletec/authentication/authentication_bloc.dart';
+import 'package:eletec/sub.dart';
 import 'package:eletec/view/ad/bloc/ad_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -6,38 +7,20 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'app.dart';
 import 'locale/locale_bloc.dart';
 
-class SimpleBlocDelegate extends BlocDelegate {
-  @override
-  void onEvent(Bloc bloc, Object event) {
-    super.onEvent(bloc, event);
-    print('onEvent:' + '$event');
-  }
+// void main() {
+//   // BlocSupervisor.delegate = SimpleBlocDelegate();
 
-  @override
-  void onTransition(Bloc bloc, Transition transition) {
-    super.onTransition(bloc, transition);
-    print('onTransition:' + '$transition');
-  }
+//   runApp(MultiBlocProvider(providers: [
+//     BlocProvider<AuthenticationBloc>(
+//       create: (_) => AuthenticationBloc()..add(AppStarted()),
+//     ),
+//     BlocProvider<LocaleBloc>(
+//       create: (_) => LocaleBloc()..add(LocaleInit()),
+//     ),
+//     BlocProvider<AdBloc>(
+//       create: (_) => AdBloc()..add(AdEvent(5)),
+//     )
+//   ], child: EletecApp()));
+// }
 
-  @override
-  void onError(Bloc bloc, Object error, StackTrace stacktrace) {
-    super.onError(bloc, error, stacktrace);
-    print('onError:' + error);
-  }
-}
-
-void main() {
-  // BlocSupervisor.delegate = SimpleBlocDelegate();
-
-  runApp(MultiBlocProvider(providers: [
-    BlocProvider<AuthenticationBloc>(
-      create: (_) => AuthenticationBloc()..add(AppStarted()),
-    ),
-    BlocProvider<LocaleBloc>(
-      create: (_) => LocaleBloc()..add(LocaleInit()),
-    ),
-    BlocProvider<AdBloc>(
-      create: (_) => AdBloc()..add(AdEvent(5)),
-    )
-  ], child: EletecApp()));
-}
+void main() => runApp(MyApp());
