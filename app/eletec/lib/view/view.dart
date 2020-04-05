4d2b43e2b0bb80f45f6d941649ebbfa19f3546c9
marscj <mjx_cj@163.com>
2020-04-05 20:27:59 +0400

    commit

--- app/eletec/lib/view/view.dart
+++ app/eletec/lib/view/view.dart
@@ -1,6 +1,6 @@
 export 'ad/ad.dart';
 export 'home/home.dart';
-export 'login/login.dart';
+export 'login_old/login.dart';
 export 'setting/setting.dart';
 // export 'widgets/widgets.dart';
 // export 'loading/loading.dart';
