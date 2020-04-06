a639d25dd3f041d7f07d1dfff431a1859c68cbcb
marscj <mjx_cj@163.com>
2020-04-06 14:54:55 +0400

    commit

--- app/eletec/lib/view/login/widgets/auth_card.dart
+++ app/eletec/lib/view/login/widgets/auth_card.dart
@@ -1,6 +1,7 @@
 
 import 'package:animator/animator.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class AuthCard extends StatefulWidget {
 
@@ -19,20 +20,30 @@ class _AuthCardState extends State<AuthCard> {
       height: deviceSize.height,
       width: deviceSize.width,
       child: Center( 
-          child: Text('card') 
+        child: FlutterLogo(
+          size: 200, 
+          colors: Colors.yellow ,
+        )
       )
     );
 
-    return Animator<double>(
-      tween: Tween<double>(begin: 0, end: 300),
-      cycles: 0,
-      builder: (anim) => Center(
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: 10),
-          height: anim.value,
-          width: anim.value,
-          child: FlutterLogo(),
-        ),
+    return Animator(
+      duration: Duration(seconds: 1),
+      tweenMap: {
+        'rotateZ': Tween<double>(begin: 0.0, end: pi/2),
+        'scale': Tween<double>(begin: 1.0, end: .2),
+        'size': Tween<double>(begin: 1, end: 1),
+        'offset': Tween<Offset>(begin: Offset.zero, end: Offset(2, 0))
+      },
+      cycles: 5,
+      // repeats: 5,   
+      builderMap: (anim) => Transform(
+        alignment: Alignment.center,
+        transform: Matrix4.identity()
+          ..rotateY(anim['rotateZ'].value)
+          ..scale(anim['scale'].value, anim['scale'].value)
+          ..scale(anim['size'].value, anim['size'].value),
+        child: container,
       ),
     );
   }
